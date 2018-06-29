24b32b94cf27bf72d58573b72560fbedd330a6a8
Ivan.Arevkov <ivan.arevkov@sidenis.com>
2018-06-29 16:24:26 +0300

    Add Modal component (global behavior encapsulation)

---
+++ 03_encap-behavior-escape-modal/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <button type="button"
+            class="btn btn-blue"
+            @click="modalOpen = true"
+    >
+      Open Modal
+    </button>
+
+    <AnnouncementModal :show="modalOpen"
+                       @close="modalOpen = false"
+    />
+  </div>
+</template>
+
+<script>
+import AnnouncementModal from './components/AnnouncementModal'
+
+export default {
+  name: 'app',
+  components: {
+    AnnouncementModal
+  },
+  data() {
+    return {
+      modalOpen: false
+    }
+  }
+}
+</script>
+
+<style src="./assets/css/app.css"></style>
