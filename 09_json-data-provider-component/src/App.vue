8d2908654600380d124f4cfe97f47766f4f66395
forest-shadow <mamasay@yandex.ru>
2018-07-05 16:17:49 +0300

    Add initial markup for data provider component creation

---
+++ 09_json-data-provider-component/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-sm mx-auto">
+      <ContactList/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContactList from './components/ContactList'
+
+export default {
+  name: 'app',
+  components: {
+    ContactList
+  }
+}
+</script>
+
+<style src="./assets/css/app.css"></style>
