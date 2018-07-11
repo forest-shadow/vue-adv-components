27e7c563713ff8398ab37de4abb38151efad0444
forest-shadow <mamasay@yandex.ru>
2018-07-11 13:47:42 +0300

    Implement data provider component

--- 09_json-data-provider-component/src/App.vue
+++ 09_json-data-provider-component/src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app" class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-sm mx-auto mb-8">
+      <FetchJson url="/albums.json">
+        <div class="card" slot-scope="{ json: albums, loading }">
+          <h1 class="text-2xl font-bold mb-6">Top Death Metal Albums</h1>
+          <div v-if="loading" class="text-grey-darker">
+            Loading...
+          </div>
+          <div v-else>
+            <div class="album-grid">
+              <div class="album-grid-item" v-for="album in albums" :key="album.id">
+                <img :src="album.artwork" class="" alt="">
+                <h2 class="album-title">{{ album.title }}</h2>
+                <p class="album-artist">{{ album.artist }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </FetchJson>
+    </div>
     <div class="max-w-sm mx-auto">
-      <ContactList/>
+      <FetchJson url="/contacts.json">
+        <div class="card" slot-scope="{ json: contacts, loading }">
+          <h1 class="text-2xl font-bold mb-6">Your Contacts</h1>
+          <div v-if="loading">Loading...</div>
+          <div v-else>
+            <div v-for="contact in contacts" :key="contact.id" class="flex items-center spaced-y-6">
+              <img :src="contact.picture.medium" class="h-12 w-12 rounded-full block mr-2" alt="">
+              <div>
+                <div class="font-bold">
+                  {{ contact.name.first }} {{ contact.name.last }}
+                </div>
+                <div class="text-grey-dark">
+                  {{ contact.email }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </FetchJson>
     </div>
   </div>
 </template>
 
 <script>
-import ContactList from './components/ContactList'
+import FetchJson from './components/FetchJson'
 
 export default {
   name: 'app',
   components: {
-    ContactList
+    FetchJson
   }
 }
 </script>
---
+++ 09_json-data-provider-component/src/components/FetchJson.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'FetchJson',
+  props: ['url'],
+  data() {
+    return {
+      json: null,
+      loading: true
+    }
+  },
+  created() {
+    axios.get(this.url)
+      .then(response => {
+        setTimeout(() => {
+          this.json = response.data
+          this.loading = false
+        }, 2000)
+      })
+  },
+  render() {
+    return this.$scopedSlots.default({
+      json : this.json,
+      loading: this.loading
+    })
+  }
+}
+</script>
