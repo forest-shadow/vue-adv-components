836b88c489e9d2f7976d8e4b6a99d796e4ab8dca
forest-shadow <mamasay@yandex.ru>
2018-07-12 10:36:29 +0300

    Add initial code for creating renderless TagInput component

---
+++ 10_tag-renderless-component/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app" class="min-h-screen bg-grey-darker p-8">
+    <div class="card max-w-sm mx-auto mt-8">
+      <label class="form-label mb-2">Renderless Tag Input</label>
+      <RenderlessTagInput>
+        <div class="tag-input" slot-scope="{}">
+        <span class="tag-input-tag">
+          <span>sample tag</span>
+          <button type="button" class="tag-input-remove">
+            &times;
+          </button>
+        </span>
+          <input class="tag-input-text"
+                 placeholder="Add tag..."
+          />
+        </div>
+      </RenderlessTagInput>
+    </div>
+
+    <div class="card max-w-sm mx-auto mt-8">
+      <label class="form-label mb-2">Original Tag Input</label>
+      <TagInput v-model="tags"></TagInput>
+    </div>
+  </div>
+</template>
+
+<script>
+import TagInput from './components/TagInput'
+import RenderlessTagInput from './components/RenderlessTagInput'
+
+export default {
+  name: 'app',
+  components: {
+    TagInput,
+    RenderlessTagInput
+  },
+  data() {
+    return {
+      tags: ['awesome', 'excellent', 'amazing']
+    }
+  }
+}
+</script>
+
+<style src="./assets/css/app.css"></style>
