83b4099dc536e0ce64b53010d3454765887f9043
forest-shadow <mamasay@yandex.ru>
2018-09-03 16:54:54 +0300

    Add example of creating element query component

---
+++ 11_element-query-card-component/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="min-h-screen bg-grey-darker p-4">
+    <div style="width: 420px" class="mx-auto border-l border-r border-dashed border-grey-dark py-4 mb-4">
+      <ProfileCard />
+    </div>
+    <div style="width: 300px" class="border-l border-r border-dashed border-grey-dark py-4 mx-auto">
+      <ProfileCard />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileCard from './components/ProfileCard'
+
+export default {
+  name: 'app',
+  components: {
+    ProfileCard
+  }
+}
+</script>
+
+<style src="./assets/css/app.css" />
---
+++ 11_element-query-card-component/src/components/WithDimensions.vue
@@ -0,0 +1,32 @@
+<script>
+import elementResizeDetectorMaker from 'element-resize-detector'
+
+// initialize erd with up-to-date strategy mode
+const erd = elementResizeDetectorMaker({ strategy: 'scroll' })
+
+export default {
+  name: 'WithDimensions',
+
+  data() {
+    return {
+      width: null,
+      height: null
+    }
+  },
+
+  mounted() {
+    // make erd able to track element's width and height
+    erd.listenTo(this.$el, (el) => {
+      this.width = el.offsetWidth
+      this.height = el.offsetHeight
+    })
+  },
+
+  render() {
+    return this.$scopedSlots.default({
+      width: this.width,
+      height: this.height
+    })
+  }
+}
+</script>
