2e38a52ed40cf3f85b3dcad0dc7bd0e9153c7579
forest-shadow <mamasay@yandex.ru>
2018-11-26 15:37:20 +0300

    Add compound sortable list component

---
+++ 13_compound-sortable-list/src/components/SortableItem.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    inject: ['sortableListItemClass'],
+    name: 'SortableItem',
+    mounted() {
+      this.$el.classList.add(this.sortableListItemClass)
+    },
+    render() {
+      return this.$slots.default[0]
+    }
+  }
+</script>
